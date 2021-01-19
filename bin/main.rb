217bd659618b78ad5eb4eef398bbdb245ee988b6
Jebitok <sharonkosgei4@gmail.com>
2021-01-19 18:10:42 +0300

    add fix linter

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/ModuleLength
 
 puts 'Welcome to the Tic Tac Toe game!'
 # create Players class initialized with name and symbol
@@ -18,18 +17,23 @@ puts '| 4 | 5 | 6 |'
 puts '--------------'
 puts '| 7 | 8 | 9 |'
 
-# Show players conditions (can't pick already picked number or invalid number(not within 1-9))
+# Show players conditions (can't pick already picked number or invalid number
+# (not within 1-9))
 # game_on = true
 puts "It is your turn #{player1}, Select your move: "
 # positionx = gets.chomp
-# Check if #{Player1} makes a valid move. If not, give a warning and repeat the move selection.
-# "if #{player1} move is invalid. Make a valid move by picking a number between (1 - 9)."
+# Check if #{Player1} makes a valid move. If not, give a warning and repeat the
+# move selection.
+# "if #{player1} move is invalid. Make a valid move by picking a number
+# between (1 - 9)."
 puts "I will mark user's picked move and display updated board"
 
 puts "It is your turn #{player2}, Enter your move: "
 # position0 = gets.chomp
-# Check if #{Player2} makes a valid move. If not, give a warning and repeat the move selection.
-# "Note: if #{player2} move is invalid. Make a valid move by picking a number between (1 - 9)."
+# Check if #{Player2} makes a valid move. If not, give a warning and repeat the
+# move selection.
+# "Note: if #{player2} move is invalid. Make a valid move by picking a number
+#  between (1 - 9)."
 puts "I will mark user's picked move and display updated board"
 puts 'Game is over! Player (either player1 or player2) is the winner!'
 # or it will output:
@@ -42,6 +46,5 @@ puts 'Game is over! The game is a draw!'
 puts 'Do you want to play again(Y/n)?'
 play_again = gets.chomp
 puts play_again
-# "The game starts again and repeats same steps if players selects #{play_again} or ends here!""
-
-# rubocop:enable Metrics/ModuleLength
+# "The game starts again and repeats same steps if players selects #{play_again}
+# or ends here!""
