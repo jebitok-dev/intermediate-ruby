5f92593df8f9354afd7350de1f7c5b10c9c07c4a
Jebitok <sharonkosgei4@gmail.com>
2021-01-19 18:00:31 +0300

    add fix linter

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/LineLength
 
 puts 'Welcome to the Tic Tac Toe game!'
 # create Players class initialized with name and symbol
@@ -18,27 +19,29 @@ puts '--------------'
 puts '| 7 | 8 | 9 |'
 
 # Show players conditions (can't pick already picked number or invalid number(not within 1-9))
-game_on = true 
+# game_on = true
 puts "It is your turn #{player1}, Select your move: "
 # positionx = gets.chomp
 # Check if #{Player1} makes a valid move. If not, give a warning and repeat the move selection.
-# "if #{player1} move is invalid. Make a valid move by picking a number between 1 - 9."
+# "if #{player1} move is invalid. Make a valid move by picking a number between (1 - 9)."
 puts "I will mark user's picked move and display updated board"
 
 puts "It is your turn #{player2}, Enter your move: "
 # position0 = gets.chomp
 # Check if #{Player2} makes a valid move. If not, give a warning and repeat the move selection.
-# "Note: if #{player2} move is invalid. Make a valid move by picking a number between 1 - 9."
+# "Note: if #{player2} move is invalid. Make a valid move by picking a number between (1 - 9)."
 puts "I will mark user's picked move and display updated board"
-puts "Game is over! Player (either player1 or player2) is the winner!"
+puts 'Game is over! Player (either player1 or player2) is the winner!'
 # or it will output:
-puts "Game is over! The game is a draw!"
+puts 'Game is over! The game is a draw!'
 
 # Continue the loop until one player wins or the game ends with a draw
 # Winner is declared or a Draw is declared
-game_on = false
+# game_on = false
 
 puts 'Do you want to play again(Y/n)?'
 play_again = gets.chomp
 puts play_again
-# The game starts again and repeats same steps if players selects #{play_again} or ends here!
+# "The game starts again and repeats same steps if players selects #{play_again} or ends here!""
+
+# rubocop:enable Metrics/LineLength
