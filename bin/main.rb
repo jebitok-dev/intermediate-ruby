c120b7a07c25bcc56a06e1c38e76159348a41bc6
Jebitok <sharonkosgei4@gmail.com>
2021-01-19 14:16:15 +0300

    add required changes

--- bin/main.rb
+++ bin/main.rb
@@ -17,20 +17,22 @@ puts '| 4 | 5 | 6 |'
 puts '--------------'
 puts '| 7 | 8 | 9 |'
 
-puts "It is your turn #{player1}, select your move: "
-# I will save the move and add the symbol
-# I will update the board and diplay it again
-# I will check the conditions of the game
-puts "It is your turn #{player2}, select your move: "
-# I will save the move and add the symbol
-# I will update the board and diplay it again
-# I will check the conditions of the game
+puts "It is your turn #{player1}, Select your move: "
+positionx = gets.chomp
+# I will save the position and add the symbol
+# I will display updated board after player's move
+# I will check the conditions of the game(If user selects valid/invalid move)
+puts "It is your turn #{player2}, Enter your move: "
+position0 = gets.chomp
+# I will save the position and add the symbol
+# I will display updated board after player's move
+# I will check the conditions of the game(If user selects valid/invalid move)
 
-puts 'Game is over! Player (either player1 or player2) is the winner!'
+puts "Game is over! Player (either player1 or player2) is the winner!"
 # or it will output:
-puts 'Game is over! The game is a draw!'
+puts "Game is over! The game is a draw!"
 
 puts 'Do you want to play again(Y/n)?'
 play_again = gets.chomp
 puts play_again
-# The game starts again or if players select #{play_again} or ends here!
+# The game starts again and repeats same steps if players selects #{play_again} or ends here!
